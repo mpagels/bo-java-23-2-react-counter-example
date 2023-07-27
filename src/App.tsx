15b076054121230cf8df59962d01914bfbd4c2d3
mpagels <54703643+mpagels@users.noreply.github.com>
2023-07-27 11:18:44 +0200

    counter end session

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState<number>(0)
-
-  function handleOnClickPlus1() {
-    setCount(count + 1)
-  }
-  function handleOnClickMinus1() {
-    setCount(count - 1)
-  }
+function Counter() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="Counter">
+    <>
       <h1>Counter</h1>
       <h2>{count}</h2>
-      <div>
-        <button onClick={handleOnClickPlus1}>Add +1</button>
-        <button onClick={handleOnClickMinus1}>Add -1</button>
-      </div>
+      <button
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        Click me
+      </button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <div className="Counter">
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   )
 }
